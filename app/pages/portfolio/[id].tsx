1df86c94e12702211faabf2067caffbb9a442805
Jonathan Adithya <jo.adithya391@gmail.com>
2023-01-26 16:35:44 -0800

    Add logs to the dynamic portfolio routes

--- app/pages/portfolio/[id].tsx
+++ app/pages/portfolio/[id].tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
 const PortfolioInfo = () => {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+  
+
   return (
     <div>
       <h1>Portfolio Info Page</h1>
